1ffd31b9e8667ace74604b7b11b4c9402a1f5395
urosaar <oussra061@gmail.com>
2024-12-26 17:42:41 +0100

    adding rev_watr and rostring

---
+++ lv4/rostring.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+
+int is_space(char c)
+{
+    return (c == ' ' || c == '\t');
+}
+
+int main(int argc, char **argv)
+{
+    if (argc >= 2)
+    {
+        int i = 0;
+        int word_start = 0;
+        int first_word_end = 0;
+
+        while (is_space(argv[1][i]))
+            i++;
+        word_start = i;
+        while (argv[1][i] && !is_space(argv[1][i]))
+            i++;
+        first_word_end = i;
+
+        while (argv[1][i])
+        {
+            while (argv[1][i] && is_space(argv[1][i]))
+                i++;
+            if (argv[1][i])
+            {
+                if (word_start != first_word_end)
+                    write(1, " ", 1);
+                while (argv[1][i] && !is_space(argv[1][i]))
+                    write(1, &argv[1][i++], 1);
+            }
+        }
+
+        if (word_start != first_word_end)
+            write(1, " ", 1);
+        while (word_start < first_word_end)
+            write(1, &argv[1][word_start++], 1);
+
+        write(1, "\n", 1);
+    }
+    else
+    {
+        write(1, "\n", 1);
+    }
+    return 0;
+}
+
+
+
