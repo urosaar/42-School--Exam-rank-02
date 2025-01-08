ffeb7c9553b0ba19f0b76ff524825ba4ba636e5a
oussama khanfar <165305673+urosaar@users.noreply.github.com>
2025-01-08 18:49:08 +0100

    Update ft_atoi_base.c

--- lv3/ft_atoi_base.c
+++ lv3/ft_atoi_base.c
@@ -4,7 +4,7 @@ int ft_atoi_base(const char *str, int str_base)
     int i = 0;
     int result = 0;
     int sign = 1;
-    const char *digits = "0123456789abcdef"; 
+    int c;
 
     while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
         i++;
@@ -15,22 +15,19 @@ int ft_atoi_base(const char *str, int str_base)
     }
     else if (str[i] == '+')
         i++; 
-    while (str[i])
-    {
-        char c = str[i];
-        int digit_value = -1;
-        if (c >= '0' && c <= '9')
-            digit_value = c - '0';
-        else if (c >= 'a' && c <= 'f')
-            digit_value = c - 'a' + 10;
-        else if (c >= 'A' && c <= 'F')
-            digit_value = c - 'A' + 10;
-        if (digit_value >= 0 && digit_value < str_base)
-            result = result * str_base + digit_value;
-        else
-            break;
-        i++;
-    }
-    return result * sign;
+	while (str[i])
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			c = str[i] - '0';
+		else if (str[i] >= 'a' && str[i] <= 'f')
+			c = str[i] - 'a' + 10;
+		else if (str[i] >= 'A' && str[i] <= 'F')
+			c = str[i] - 'A' + 10;
+		else
+			break;
+		res = res * str_base + c;
+		i++;
+	}
+	return (res * sign);
 }
 
